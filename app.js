166b63e0e0fd116bfb0f3f99052d53eb536d230e
ongyh123 <105908156+ongyh123@users.noreply.github.com>
2023-01-07 23:27:55 +0800

    To determine the left %

--- app.js
+++ app.js
@@ -2,3 +2,19 @@
 const slides = document.querySelectorAll(".slide");
 const nextBtn = document.querySelector(".nextBtn");
 const prevBtn = document.querySelector(".prevBtn");
+
+// To determine the left %
+slides.forEach(function (slide, index) {
+    slide.style.left = `${index * 100}%`;
+});
+
+// 1st item, index = 0, 0 * 100 = 0%
+// 2nd item, index = 1, 1 * 100 = 100%
+// 3rd item, index = 2, 2 * 100 = 200%
+// 4th item, index = 3, 3 * 100 = 300%
+
+// <style>
+//      slide { 
+//          left: 100%;
+//      }
+// </style>
